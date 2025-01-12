5931a02340c6ae11e0e6fb228292b3414e0ac14c
Bohdan Pysaniuk <pusanukbr@gmail.com>
2025-01-12 17:14:35 +0100

    delete any files

---
+++ frontend/src/pages/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div
+        class="flex justify-center h-full w-full items-center bg-white dark:bg-gray-900"
+    >
+        <div
+            class="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 max-w-md w-full"
+        >
+            <LoginForm />
+            <p
+                class="text-gray-800 dark:text-gray-100 mt-4 text-center text-sm"
+            >
+                {{ t("dontHaveAccount") }}
+                <BaseLink to="/register">{{ t("registerHere") }}</BaseLink>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import LoginForm from "../components/LoginForm.vue";
+import BaseLink from "../UI/BaseLink.vue";
+
+import { useI18n } from "vue-i18n";
+
+// Використовуємо i18n
+const { t } = useI18n();
+</script>
---
+++ frontend/src/pages/Register.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex justify-center items-center bg-white dark:bg-gray-900">
+        <div
+            class="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 max-w-md w-full"
+        >
+            <RegisterForm />
+            <p
+                class="text-gray-800 dark:text-gray-100 mt-4 text-center text-sm"
+            >
+                {{ t("alreadyHaveAccount") }}
+                <BaseLink to="/login">{{ t("loginHere") }}</BaseLink>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import RegisterForm from "../components/RegisterForm.vue";
+import BaseLink from "../UI/BaseLink.vue";
+
+import { useI18n } from "vue-i18n";
+
+// Використовуємо i18n
+const { t } = useI18n();
+</script>
